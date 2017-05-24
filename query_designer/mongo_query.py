c3762190444fed3f9618c94a9c3b97f5b2320ca7
dipapaspyros <dipapaspyros@gmail.com>
2017-05-24 18:42:49 +0300

    Minor updates in mongo_query

--- query_designer/mongo_query.py
+++ query_designer/mongo_query.py
@@ -12,91 +12,34 @@ from bson import ObjectId
 
 def execute_query(request):
 
-    def matches(query_document, d):
-        return d['value'] is not None
-
-
     db = get_mongo_db()
     query_document = json.loads(request.POST.get('query'), '')
 
     # use from to filter dataset
-    filters = {
-        'variable_id': ObjectId(query_document['from'][0]['type']['id']),
-    }
+    filters = {}
 
     # fetch variables and their dimensions
     variable = db.variables.find_one({
-        '_id': ObjectId(query_document['from'][0]['type']['id']),
+        '_id': ObjectId(query_document['from'][0]['type']),
     })
-    dimensions = db.variables.aggregate([
-        {"$match": {'_id': ObjectId(query_document['from'][0]['type']['id'])}},
-        {"$unwind": "$dimension_ids"},
-        {"$lookup": {
-            "from": "dimensions",
-            "localField": "dimension_ids",
-            "foreignField": "_id",
-            "as": "dimensions"
-        }},
-        {"$unwind": "$dimensions"},
-        {"$group": {
-            "_id": "$_id",
-            "dimension_ids": {"$push": "$dimension_ids"},
-            "dimensions": {"$push": "$dimensions"}
-        }}
-    ]).next()['dimensions']
 
-    documents = db.data.find(filters)
+    documents = db.get_collection(variable['name']).find(filters)
 
     result = []
-    match_idx = -1
 
-    # read offset & limit
-    try:
+    # offset & limit
+    offset = None
+    limit = None
+    if 'offset' in query_document and query_document['offset']:
         offset = int(query_document['offset'])
-    except (ValueError, KeyError, TypeError):
-        offset = 0
-    try:
+
+    if 'limit' in query_document and query_document['limit']:
         limit = int(query_document['limit'])
-    except (ValueError, KeyError, TypeError):
-        limit = 100
 
-    finish = False
-    for document in documents:
-        if finish:
-            break
+    if offset:
+        documents = documents.skip(offset)
 
-        data = document['values']
-        dim_values = []
-        for dimension in dimensions:
-            vv = []
-            v = float(dimension['min'])
-            idx = 0
-            while v <= float(dimension['max']):
-                vv.append((idx, v))
-                if dimension['step'] is None:
-                    break
-                idx += 1
-                v += float(dimension['step'])
-            dim_values.append(vv)
+    if limit:
+        documents = documents.limit(limit)
 
-        for comb in itertools.product(*dim_values):
-            document = {}
-            dt = data
-            for idx, dimension in enumerate(comb):
-                document[dimensions[idx]['name']] = comb[idx][1]
-                dt = dt[comb[idx][0]]
-
-            document['value'] = dt
-
-            if matches(query_document, document):
-                match_idx += 1
-
-                if match_idx >= offset:
-                    result.append(document)
-
-                if match_idx >= limit:
-                    finish = True
-                    break
-
-        # calculate how many matches there are based on the filters
     return MongoResponse(result, safe=False)
