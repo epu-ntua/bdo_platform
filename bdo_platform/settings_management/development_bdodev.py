864a6513a5c5b5b19a25f731dcf19c3cd993cc88
dipapaspyros <dipapaspyros@gmail.com>
2017-06-23 18:09:08 +0300

    Minor settings fix

--- bdo_platform/settings_management/development_bdodev.py
+++ bdo_platform/settings_management/development_bdodev.py
@@ -1,4 +1,4 @@
-from bdo_platform.settings_management.base_settings import *
+from bdo_platform.settings_management.development import *
 
 SPARK_SUBMIT_PATH = '/usr/lib/spark/bin/spark-submit'
 SERVER_URL = 'http://bdo-dev.epu.ntua.gr'
