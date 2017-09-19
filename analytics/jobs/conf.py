1437004982ca31af6f1050cc5d0d87ac18e2ac32
dipapaspyros <dipapaspyros@gmail.com>
2017-09-19 17:53:29 +0300

    Separated config db connection from linear regression logic

---
+++ analytics/jobs/conf.py
@@ -0,0 +1,4 @@
+db = {
+    'psycopg': "dbname='bdo_platform' user='postgres' host='localhost' password='bdo!'",
+    'jdbc': "dbname='bdo_platform' user='postgres' host='localhost' password='1234'",
+}
