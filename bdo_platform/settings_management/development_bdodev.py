746dbfa77e755bce87e071ad1c027180e14a947b
dipapaspyros <dipapaspyros@gmail.com>
2017-09-21 14:55:30 +0300

    Minor fix

--- bdo_platform/settings_management/development_bdodev.py
+++ bdo_platform/settings_management/development_bdodev.py
@@ -1,7 +1,7 @@
 from bdo_platform.settings_management.development import *
 
-DATABASES['default']['user'] = 'postgres'
-DATABASES['default']['password'] = 'bdo!'
+DATABASES['default']['USER'] = 'postgres'
+DATABASES['default']['PASSWORD'] = 'bdo!'
 
 SPARK_SUBMIT_PATH = '/usr/lib/spark/bin/spark-submit'
 SERVER_URL = 'http://bdo-dev.epu.ntua.gr'
