ebac4bd6ae44c79bdcea7dfb1e6d1aa88402e74c
gtsapelas <gtsapelas@epu.ntua.gr>
2018-02-05 23:25:47 +0200

    fixed errors in map course time visualization

--- visualizer/tests.py
+++ visualizer/tests.py
@@ -8,19 +8,19 @@ def trim_year(full_date):
     return int(year[0])
 
 def get_data(markers, ship, minyear, maxyear):
-    q = Query.objects.get(pk=2)
+    q = Query.objects.get(pk=75)
     q.document['limit'] = markers
     #import pdb;pdb.set_trace()
-    result = q.execute()['results']
+    result = q.execute()[0]['results']
     length = len(result) - 1
     data = []
     for index in range(0, length):
         d = result[index]
-        entry_year = trim_year(d[2])
+        entry_year = trim_year(d[4])
         if ship != "all":
             ship = int(ship)
             if d[1] != ship:
                 continue
         if entry_year >= minyear or entry_year <= maxyear:
-            data.append([float(d[3]), float(d[4]), int(d[1]), str(d[2]), float(d[0])])
+            data.append([float(d[2]), float(d[3]), int(d[1]), str(d[4]), float(d[0])])
     return data
