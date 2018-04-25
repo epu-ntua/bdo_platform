7fe8aaf73eca569976334a751b40fc9d1124e294
epu_temp_steve <sskalidakis@epu.ntua.gr>
2018-04-25 13:57:50 +0300

    Merge branch 'master' of https://github.com/epu-ntua/bdo_platform
    # Conflicts:
    #	analytics/templates/basic-analytics/config-analysis-form-fields.html
    #	analytics/templates/service_builder/service_builder.html
    #	dashboard_builder/templates/dashboard_builder/config-visualization-form-fields.html
    #	service_builder/static/service_builder/js/arguments_tab.js
    #	service_builder/static/service_builder/js/publish.js
    #	service_builder/templates/service_builder/arguments_tab.html

---
+++ analytics/static/js/datepicker.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    /*          Set Up Time Pickers For Start/End Date  */
+    var startpick = $('#startdatepicker').datetimepicker({autoclose: true, pickerPosition: 'top-right'});
+    var endpick = $('#enddatepicker').datetimepicker({autoclose: true, pickerPosition: 'top-right'});
+
+    startpick.on('changeDate', function(e){
+        var minDate = new Date(e.date.valueOf());
+        endpick.datetimepicker('setStartDate' ,minDate);
+        startdate = $('#startdatepicker input').val();
+        // set_time_filters();
+    });
+    endpick.on('changeDate', function(e){
+        var maxDate = new Date(e.date.valueOf());
+        startpick.datetimepicker('setEndDate', maxDate);
+        enddate = $('#enddatepicker input').val();
+        // set_time_filters();
+    });
+    /*          Set Up Time Pickers For Start/End Date  */
+
+
+
+    $('#project').on('click', function(){
+        console.log(startdate, enddate);
+        // startdate and enddate must be transposed to match database format
+        QueryToolbox.filterManager.addFilter('timestamp', 'gt' , startdate.toString());
+        QueryToolbox.filterManager.addFilter('timestamp', 'lt' , enddate.toString());
+    });
+
+
+    // function set_time_filters() {
+    //     var time_dim_id = $('#selected_dimensions option[data-type="time"]').val();
+    //
+    // }
+
+
+});
