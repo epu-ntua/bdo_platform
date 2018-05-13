9f0b645cce09e30aa40e120bd23c764b639cec72
gtsapelas <gtsapelas@epu.ntua.gr>
2018-05-13 17:37:30 +0300

    distinguish variables with the same canonical name

--- query_designer/static/query_designer/js/simplified/toolbox.js
+++ query_designer/static/query_designer/js/simplified/toolbox.js
@@ -811,15 +811,15 @@ $(function () {
             $.each(info.values, function(idx, value) {
                 var _from = {
                     type: value.type,
-                    name: value.name + '_' + String(idx),
+                    name: String(value.name).split("_pk_")[1] + '_' + String(idx),
                     select: []
                 };
 
                 // push main value
                 _from.select.push({
                    type: 'VALUE',
-                   name: 'i' + String(idx) + '_' + value.name,
-                   title: value.name,
+                   name: 'i' + String(idx) + '_' + String(value.name).split("_pk_")[1],
+                   title: String(value.name).split("_pk_")[1],
                    aggregate: value.aggregate,
                    groupBy: false,
                    exclude: false
@@ -937,7 +937,7 @@ $(function () {
                         var iframe = $('<iframe  onload="hide_gif();" />');
                         $("#viz_container iframe").remove();
                         $("#viz_container").append(iframe);
-                        $('#viz_container iframe').attr('src', 'http://localhost:8000/visualizations/get_line_chart_am/?query=' + id + '&y_var=' + y_var + '&x_var=' + x_var);
+                        $('#viz_container iframe').attr('src', 'http://localhost:8000/visualizations/get_line_chart_am/?query=' + id + '&y_var[]=' + y_var + '&x_var=' + x_var);
 
 
 
