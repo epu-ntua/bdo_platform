b73928ed4c50469e26bec22cab74d0adebeadada
gtsapelas <gtsapelas@epu.ntua.gr>
2018-10-25 17:53:38 +0300

    Removing ignored files

---
+++ bdo_platform/settings_management/git_bash.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git for-each-ref --shell --format="ref=%(refname)" refs/heads | \
+while read entry
+do
+	git rm -r --cached development*
+	git add -A
+	git commit -am 'Removing ignored files'
+done
