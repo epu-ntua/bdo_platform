ce7ca7fd6af578dcdff7c44b3cef1f1e38eb9687
gtsapelas <gtsapelas@epu.ntua.gr>
2018-12-02 12:28:03 +0200

    visulaisations to service builder from query

--- service_builder/static/service_builder/js/output_tab.js
+++ service_builder/static/service_builder/js/output_tab.js
@@ -198,183 +198,183 @@ function show_hide_results(){
     });
 
 
-	$('#addVizModal select').select2();
+	$('#addVizModal #load-viz-query-select').select2();
 
 
-    function updateVariables(element) {
-        $('#addVizModal .variable-select').find('option').remove().end();
-        $('#addVizModal .variable-select').append($("<option disabled selected>-- select variable --</option>"));
-        var new_query_id = $('#addVizModal #selected_query').val();
-        var new_query_doc = {};
-        $.ajax({
-            url: '/queries/get_query_variables/',
-            data: {
-                id: new_query_id,
-                document: new_query_doc
-                },
-            type: 'GET',
-            success: function(result){
-                var variables = result['variables'];
-                var dimensions = result['dimensions'];
-                $('.variable-select').append($("<option></option>")
-                .attr("value", '')
-                .text('-- column select --'));
+    // function updateVariables(element) {
+    //     $('#addVizModal .variable-select').find('option').remove().end();
+    //     $('#addVizModal .variable-select').append($("<option disabled selected>-- select variable --</option>"));
+    //     var new_query_id = $('#addVizModal #selected_query').val();
+    //     var new_query_doc = {};
+    //     $.ajax({
+    //         url: '/queries/get_query_variables/',
+    //         data: {
+    //             id: new_query_id,
+    //             document: new_query_doc
+    //             },
+    //         type: 'GET',
+    //         success: function(result){
+    //             var variables = result['variables'];
+    //             var dimensions = result['dimensions'];
+    //             $('.variable-select').append($("<option></option>")
+    //             .attr("value", '')
+    //             .text('-- column select --'));
+    //
+    //             $.each(variables, function(k, v) {
+    //                 $('#addVizModal .variable-select').append($("<option></option>")
+    //                     .attr("value", v)
+    //                     .text(k));
+    //             });
+    //
+    //             $.each(dimensions, function(k, v) {
+    //                 $('#addVizModal .variable-select').append($("<option></option>")
+    //                     .attr("value", v)
+    //                     .text(k));
+    //             });
+    //         }
+    //     });
+    // }
 
-                $.each(variables, function(k, v) {
-                    $('#addVizModal .variable-select').append($("<option></option>")
-                        .attr("value", v)
-                        .text(k));
-                });
 
-                $.each(dimensions, function(k, v) {
-                    $('#addVizModal .variable-select').append($("<option></option>")
-                        .attr("value", v)
-                        .text(k));
-                });
-            }
-        });
-    }
-
-
-    $('#addVizModal .select2').on("click", function() {
-        $('.popover').popover('hide');
-    });
+    // $('#addVizModal .select2').on("click", function() {
+    //     $('.popover').popover('hide');
+    // });
 
     $('#addVizModal #query select').on('change', function() {
         $('#addVizModal #query #viz_config').show();
         var new_query_id = $(this).children(":selected").attr("data-query-id");
         $('#addVizModal #query #selected_query').val(new_query_id);
         $('.popover').popover('hide');
-          updateVariables(this);
-    });
-
-    $('#addVizModal #dataframe #selected_dataframe').on('change', function() {
-        $('#addVizModal #dataframe #viz_config').show();
-        var new_query_id = $(this).children(":selected").attr("data-query-id");
-        $('#addVizModal #selected_query').val(new_query_id);
-        $('.popover').popover('hide');
           // updateVariables(this);
     });
 
+    // $('#addVizModal #dataframe #selected_dataframe').on('change', function() {
+    //     $('#addVizModal #dataframe #viz_config').show();
+    //     var new_query_id = $(this).children(":selected").attr("data-query-id");
+    //     $('#addVizModal #selected_query').val(new_query_id);
+    //     $('.popover').popover('hide');
+    //       // updateVariables(this);
+    // });
 
 
-    $("#query .viz_item").click(function (element) {
-      var component_id = $(this).attr('data-viz-id');
-      var component_selector = 'li[data-viz-id="'+component_id+'"]';
-      $.ajax({
-            url: '/dashboards/get_visualization_form_fields',
-            data: {
-                id: parseInt(component_id),
-                order: 1
-                },
-            type: 'GET',
-            success: function(form_fields){
-                $("#conf-container").html(form_fields);
-                $("#conf-container").append('<button type="button" id="select_conf_ok" class="btn btn-sm btn-success" data-toggle="popover">OK</button>');
 
-                $(component_selector).popover({
-                    html: true,
-                    title: 'Configure visualisation',
-                    trigger: 'manual',
-                    content: function() {
-                        return $('#conf-container').html();
-                    }
-                });
-                updateVariables($('#load-viz-query-select'));
-                $(component_selector).popover('toggle');
-                var popver_id = '#' + $(component_selector).attr('aria-describedby');
-                $(popver_id+' #select_conf_ok').click(function(e){
-                    submit_conf(component_selector, 'query');
-                    $(component_selector).popover("hide");
-                });
-            }
-        });
-    });
+    // $("#query .viz_item").click(function (element) {
+    //   var component_id = $(this).attr('data-viz-id');
+    //   var component_selector = 'li[data-viz-id="'+component_id+'"]';
+    //   $.ajax({
+    //         url: '/dashboards/get_visualization_form_fields',
+    //         data: {
+    //             id: parseInt(component_id),
+    //             order: 1
+    //             },
+    //         type: 'GET',
+    //         success: function(form_fields){
+    //             $("#conf-container").html(form_fields);
+    //             $("#conf-container").append('<button type="button" id="select_conf_ok" class="btn btn-sm btn-success" data-toggle="popover">OK</button>');
+    //
+    //             $(component_selector).popover({
+    //                 html: true,
+    //                 title: 'Configure visualisation',
+    //                 trigger: 'manual',
+    //                 content: function() {
+    //                     return $('#conf-container').html();
+    //                 }
+    //             });
+    //             updateVariables($('#load-viz-query-select'));
+    //             $(component_selector).popover('toggle');
+    //             var popver_id = '#' + $(component_selector).attr('aria-describedby');
+    //             $(popver_id+' #select_conf_ok').click(function(e){
+    //                 submit_conf(component_selector, 'query');
+    //                 $(component_selector).popover("hide");
+    //             });
+    //         }
+    //     });
+    // });
 
-    $("#dataframe .viz_item").click(function (element) {
-      var component_id = $(this).attr('data-viz-id');
-      var component_selector = '#dataframe li[data-viz-id="'+component_id+'"]';
-      $.ajax({
-            url: '/dashboards/get_visualization_form_fields_df',
-            data: {
-                id: parseInt(component_id),
-                order: 1
-                },
-            type: 'GET',
-            success: function(form_fields){
-                $("#conf-container").html(form_fields);
-                $("#conf-container").append('<button type="button" id="select_conf_ok" class="btn btn-sm btn-success" data-toggle="popover">OK</button>');
-
-                $(component_selector).popover({
-                    html: true,
-                    title: 'Configure visualisation',
-                    trigger: 'manual',
-                    content: function() {
-                        return $('#conf-container').html();
-                    }
-                });
-                $(component_selector).popover('toggle');
-                // alert("submit conf");
-                var popver_id = '#' + $(component_selector).attr('aria-describedby');
-                $(popver_id+' #select_conf_ok').click(function(e){
-                    // alert("submit conf");
-                    submit_conf(component_selector, 'df');
-                    $(component_selector).popover("hide");
-                });
-            }
-        });
-    });
-
-
-    var viz_request = "";
-    function submit_conf(component_selector, from) {
-        // viz_request = "http://localhost:8000/visualizations/";
-        viz_request = "/visualizations/";
-        viz_request += $('#addVizModal').find('.modal-body').find('#action').val();
-        var conf_popover_id = '#' + $(component_selector).attr('aria-describedby');
-        // alert(viz_request);
-        var submitted_args = $('#addVizModal').find(conf_popover_id).find('.popover-content').clone();
-        var selects = $('#addVizModal').find(conf_popover_id).find('.popover-content').find("select");
-        $(selects).each(function(i) {
-            var select = this;
-            $(submitted_args).find("select").eq(i).val($(select).val());
-        });
-        $('#config-viz-form').empty();
-        $('#config-viz-form').append(submitted_args);
+    // $("#dataframe .viz_item").click(function (element) {
+    //   var component_id = $(this).attr('data-viz-id');
+    //   var component_selector = '#dataframe li[data-viz-id="'+component_id+'"]';
+    //   $.ajax({
+    //         url: '/dashboards/get_visualization_form_fields_df',
+    //         data: {
+    //             id: parseInt(component_id),
+    //             order: 1
+    //             },
+    //         type: 'GET',
+    //         success: function(form_fields){
+    //             $("#conf-container").html(form_fields);
+    //             $("#conf-container").append('<button type="button" id="select_conf_ok" class="btn btn-sm btn-success" data-toggle="popover">OK</button>');
+    //
+    //             $(component_selector).popover({
+    //                 html: true,
+    //                 title: 'Configure visualisation',
+    //                 trigger: 'manual',
+    //                 content: function() {
+    //                     return $('#conf-container').html();
+    //                 }
+    //             });
+    //             $(component_selector).popover('toggle');
+    //             // alert("submit conf");
+    //             var popver_id = '#' + $(component_selector).attr('aria-describedby');
+    //             $(popver_id+' #select_conf_ok').click(function(e){
+    //                 // alert("submit conf");
+    //                 submit_conf(component_selector, 'df');
+    //                 $(component_selector).popover("hide");
+    //             });
+    //         }
+    //     });
+    // });
 
 
-        var myData = $("#config-viz-form").serialize();
-        viz_request += '?';
-        viz_request += myData;
+    // var viz_request = "";
+    // function submit_conf(component_selector, from) {
+    //     // viz_request = "http://localhost:8000/visualizations/";
+    //     viz_request = "/visualizations/";
+    //     viz_request += $('#addVizModal').find('.modal-body').find('#action').val();
+    //     var conf_popover_id = '#' + $(component_selector).attr('aria-describedby');
+    //     // alert(viz_request);
+    //     var submitted_args = $('#addVizModal').find(conf_popover_id).find('.popover-content').clone();
+    //     var selects = $('#addVizModal').find(conf_popover_id).find('.popover-content').find("select");
+    //     $(selects).each(function(i) {
+    //         var select = this;
+    //         $(submitted_args).find("select").eq(i).val($(select).val());
+    //     });
+    //     $('#config-viz-form').empty();
+    //     $('#config-viz-form').append(submitted_args);
+    //
+    //
+    //     var myData = $("#config-viz-form").serialize();
+    //     viz_request += '?';
+    //     viz_request += myData;
+    //
+    //     if (from === 'query')
+    //         viz_request += '&query=' + $('#addVizModal #selected_query').val();
+    //     else
+    //         viz_request += '&df=' + $('#addVizModal #selected_dataframe').val();
+    //         viz_request += '&notebook_id=' + $('#notebook_id').val();
+    //     // alert(viz_request);
+    //     show_viz(viz_request, from);
+    // }
 
-        if (from === 'query')
-            viz_request += '&query=' + $('#addVizModal #selected_query').val();
-        else
-            viz_request += '&df=' + $('#addVizModal #selected_dataframe').val();
-            viz_request += '&notebook_id=' + $('#notebook_id').val();
-        // alert(viz_request);
-        show_viz(viz_request, from);
-    }
-
-    function show_viz(viz_request, from) {
-        if (from === 'query')
-            $("#query #viz_container").html('<iframe id="viz-iframe" ' +
-                'src="'+viz_request+'" frameborder="0" allowfullscreen="" '+
-                '></iframe>');
-        else
-            $("#dataframe #viz_container").html('<iframe id="viz-iframe" ' +
-                'src="'+viz_request+'" frameborder="0" allowfullscreen="" '+
-                '></iframe>');
-    }
+    // function show_viz(viz_request, from) {
+    //     if (from === 'query')
+    //         $("#query #viz_container").html('<iframe id="viz-iframe" ' +
+    //             'src="'+viz_request+'" frameborder="0" allowfullscreen="" '+
+    //             '></iframe>');
+    //     else
+    //         $("#dataframe #viz_container").html('<iframe id="viz-iframe" ' +
+    //             'src="'+viz_request+'" frameborder="0" allowfullscreen="" '+
+    //             '></iframe>');
+    // }
 
 
-    $("#addVizModal #submit-query-btn").click(function () {
-        $('#query #viz_container iframe').appendTo('#dynamic_dashboard');
-        html_editor.replaceRange('\n<div id="viz_container"><div class="loadingFrame"><img src="http://assets.motherjones.com/interactives/projects/features/koch-network/shell19/img/loading.gif"/></div><iframe src="'+viz_request+'" frameborder="0" allowfullscreen="" style="width: 100%;min-height: 500px;"></iframe></div>\n', {line: Infinity});
-        html_editor.refresh();
-        viz_request = "";
-        $('#query #addVizModal #viz_container').empty();
-    });
+    // $("#addVizModal #submit-query-btn").click(function () {
+    //     $('#query #viz_container iframe').appendTo('#dynamic_dashboard');
+    //     html_editor.replaceRange('\n<div id="viz_container"><div class="loadingFrame"><img src="http://assets.motherjones.com/interactives/projects/features/koch-network/shell19/img/loading.gif"/></div><iframe src="'+viz_request+'" frameborder="0" allowfullscreen="" style="width: 100%;min-height: 500px;"></iframe></div>\n', {line: Infinity});
+    //     html_editor.refresh();
+    //     viz_request = "";
+    //     $('#query #addVizModal #viz_container').empty();
+    // });
 
     $("#addVizModal #submit-dataframe-btn").click(function () {
         $('#dataframe #viz_container iframe').appendTo('#dynamic_dashboard');
