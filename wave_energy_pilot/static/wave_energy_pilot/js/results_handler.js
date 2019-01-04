832300b025766b6ba548321d09d47595f91a5ccd
Christos <chrisdrummer97@hotmail.com>
2019-01-04 16:15:58 +0200

    init wave pilot django app

---
+++ wave_energy_pilot/static/wave_energy_pilot/js/results_handler.js
@@ -0,0 +1,6 @@
+$('.ui.button')
+  .on('click', function() {
+    // programmatically activating tab
+    $.tab('change tab', 'tab-name');
+  })
+;
--- wave_energy_pilot/views.py
+++ wave_energy_pilot/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+
 # Create your views here.
+def init(request):
+    return render(request, 'wave_energy_pilot/load_service.html', {})
