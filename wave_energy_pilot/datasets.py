9da5ccd4e33f3da4efe5f1af7a7d610fe6a3076a
Christos <chrisdrummer97@hotmail.com>
2019-01-16 18:08:56 +0200

    datasets_load

---
+++ wave_energy_pilot/datasets.py
@@ -0,0 +1,23 @@
+
+DATASETS = (
+    dict(id="1234", title="Copernicus", variables=(dict(id=1, title="wave_period"),
+                                                   dict(id=2, title="wave_height"),
+                                                   dict(id=3, title="temperature")),
+         min_lat=36.682737, max_lat=37.155939, min_lng=16.676737, max_lng=20.691313, min_date="3/4/2018", max_date="7/5/2018"),
+    dict(id="2345", title="Maritec - 1", variables=(dict(id=1, title="wave_period"),
+                                                       dict(id=2, title="wave_height"),
+                                                       dict(id=3, title="temperature")),
+             min_lat=-40.98585, max_lat=43.385090, min_lng=-4.215340, max_lng=174.50659, min_date="7/3/2018", max_date="30/3/2018"),
+    dict(id="3456", title="Dummy Name", variables=(dict(id=1, title="wave_period"),
+                                                       dict(id=2, title="wave_height"),
+                                                       dict(id=3, title="temperature")),
+             min_lat=36.346103, max_lat=38.784063, min_lng=-7.114528, max_lng=17.616418, min_date="3/8/2018", max_date="17/9/2018"),
+    dict(id="4567", title="Dataset 4", variables=(dict(id=1, title="wave_period"),
+                                                           dict(id=2, title="wave_height"),
+                                                           dict(id=3, title="temperature")),
+                 min_lat=34.411442, max_lat=35.955777, min_lng=25.391511, max_lng=26.621469, min_date="15/10/2018", max_date="17/11/2018"),
+    dict(id="5678", title="Dataset 5", variables=(dict(id=1, title="wave_period"),
+                                                           dict(id=2, title="wave_height"),
+                                                           dict(id=3, title="temperature")),
+                 min_lat=42.484251, max_lat=42.484251, min_lng=5.668252, max_lng=5.668252, min_date="3/8/2018", max_date="17/9/2018")
+)
--- wave_energy_pilot/views.py
+++ wave_energy_pilot/views.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from lists import *
+from datasets import *
 
 # Create your views here.
 def init(request):
 
-    return render(request, 'wave_energy_pilot/load_service.html', {'buoys_list': BUOYS})
+    return render(request, 'wave_energy_pilot/load_service.html', {'buoys_list': BUOYS, 'datasets': DATASETS})
