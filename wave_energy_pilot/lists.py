5ab4b523c656d6cb9144391465097b80211824c7
Christos <chrisdrummer97@hotmail.com>
2019-01-29 14:14:28 +0200

    correct dataset selection

--- wave_energy_pilot/lists.py
+++ wave_energy_pilot/lists.py
@@ -1,14 +1,14 @@
 
 BUOYS = (
-    dict(id="7C6B07", lat=36.682737, lon=16.676737, dataset="1234"),
-    dict(id="7C6B38", lat=37.155939, lon=20.691313, dataset="1234"),
-    dict(id="7C6CA1", lat=43.385090, lon=-4.215340, dataset="2345"),
-    dict(id="7C6CA2", lat=-40.98585, lon=174.50659, dataset="2345"),
-    dict(id="C81D9D", lat=36.346103, lon=-7.114528, dataset="3456"),
-    dict(id="C82009", lat=38.784063, lon=17.616418, dataset="3456"),
-    dict(id="C82081", lat=35.955777, lon=26.621469, dataset="4567"),
-    dict(id="C820AB", lat=34.411442, lon=25.391511, dataset="4567"),
-    dict(id="C820B6", lat=42.484251, lon=5.668252,  dataset="5678")
+    dict(id="7C6B07", lat=36.682737, lon=16.676737, dataset_id="1234"),
+    dict(id="7C6B38", lat=37.155939, lon=20.691313, dataset_id="1234"),
+    dict(id="7C6CA1", lat=43.385090, lon=-4.215340, dataset_id="2345"),
+    dict(id="7C6CA2", lat=-40.98585, lon=174.50659, dataset_id="2345"),
+    dict(id="C81D9D", lat=36.346103, lon=-7.114528, dataset_id="3456"),
+    dict(id="C82009", lat=38.784063, lon=17.616418, dataset_id="3456"),
+    dict(id="C82081", lat=35.955777, lon=26.621469, dataset_id="4567"),
+    dict(id="C820AB", lat=34.411442, lon=25.391511, dataset_id="4567"),
+    dict(id="C820B6", lat=42.484251, lon=5.668252,  dataset_id="5678")
 )
 
 
