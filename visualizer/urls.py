4ceecd41d8f88b1513e4da2924cf6e211af1c2c9
epu_temp_steve <sskalidakis@epu.ntua.gr>
2019-02-08 17:10:49 +0200

    -histogram 2d new implementation

--- visualizer/urls.py
+++ visualizer/urls.py
@@ -34,7 +34,8 @@ urlpatterns = [
     url('^get_pie_chart_am/$', views.get_pie_chart_am, name='get_pie_chart_am'),
     url('^get_column_chart_am/$', views.get_column_chart_am, name='get_column_chart_am'),
     url('^get_histogram_chart_am/$', views.get_histogram_chart_am, name='get_histogram_chart_am'),
-    url('^get_histogram_2d_am/$', views.get_histogram_2d_am, name='get_histogram_2d_am'),
+    # url('^get_histogram_2d_am/$', views.get_histogram_2d_am, name='get_histogram_2d_am'),
+    url('^get_histogram_2d_am/$', views.get_histogram_2d_matplotlib, name='get_histogram_2d_am'),
 
     url('^get_data_table/$', views.get_data_table, name='get_data_table'),
     url('^get_aggregate_value/$', views.get_aggregate_value, name='get_aggregate_value'),
