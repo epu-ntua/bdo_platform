6200e512c79a8bd07f74f4eed2537e4beaa56668
Christos <chrisdrummer97@hotmail.com>
2019-04-06 14:28:54 +0300

    scenario 2 with wec input

--- wave_energy_pilot/datasets.py
+++ wave_energy_pilot/datasets.py
@@ -25,8 +25,11 @@ DATASETS = (
     #                                                        dict(id=3, title="temperatureeeeeeeeee")),
     #              min_lat=42.484251, max_lat=42.484251, min_lng=5.668252, max_lng=5.668252, min_date="03/08/2018", max_date="07/09/2018",
     #      applications=["data_visualisation"]),
-    dict(id="111", title="Maretec Wave Forecast", variables=(dict(id=1, title="sea_surface_wave_zero_upcrossing_period"),
+    dict(id="14", title="Maretec Wave Forecast", variables=(dict(id=1, title="sea_surface_wave_zero_upcrossing_period"),
                                                                dict(id=2, title="sea_surface_wave_significant_height")),
-                     min_lat=-90.00, max_lat=90.00, min_lng=-180.00, max_lng=180.00, min_date="12/01/2018", max_date="31/03/2019",
-             applications=["data_visualisation", "wave_forecast", "wave_resource_assessment_single", "wave_resource_assessment_area"])
+                     min_lat=-12.00, max_lat=12.00, min_lng=-14.00, max_lng=14.00, min_date="12/01/2018", max_date="03/31/2019",
+             applications=["data_visualisation", "wave_forecast", "wave_resource_assessment_single",
+                           "wave_resource_assessment_area", "matching_analysis", "wecs_assessment_location"])
 )
+
+# Date format: mm//dd/yyyy
